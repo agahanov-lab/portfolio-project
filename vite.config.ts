20aed49b723d2285ad06fcb0c048e85d4ac8ba0c
agahanov-lab <agahanovmekan05@gmail.com>
2025-06-16 16:42:25 -0400

    Build to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-project/',
+  build: {
+    outDir: 'docs', // <-- Add this line
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
